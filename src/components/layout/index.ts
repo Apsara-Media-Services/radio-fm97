8785486a91697dc7b85b410244e8f816aea3017f
NySominea <sominea.ny77@gmail.com>
2023-05-26 16:27:42 +0700

    style: :art: eslint

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,6 +1,5 @@
 import Footer from './Footer';
 import Header from './Header';
-import Meta from './Meta';
 import BottomMenu from './MobileBottomMenu';
 
-export { Meta, Header, Footer, BottomMenu };
+export { Header, Footer, BottomMenu };
