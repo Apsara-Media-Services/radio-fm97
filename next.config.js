ce3d591062f2d9fbcf4c2ec6d18696d0643327d2
NySominea <sominea.ny77@gmail.com>
2023-05-28 17:01:35 +0700

    fix: build

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+if (!process.env.NEXT_PUBLIC_WORDPRESS_API_URL) {
+  throw new Error(`
+    Please provide a valid WordPress instance URL.
+    Add to your environment variables WORDPRESS_API_URL.
+  `);
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
