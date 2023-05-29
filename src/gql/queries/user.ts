de83169ad213d3880b3b0238a2c7a2771b3c2746
NySominea <sominea.ny77@gmail.com>
2023-05-29 21:27:50 +0700

    chore: build

--- src/gql/queries/user.ts
+++ src/gql/queries/user.ts
@@ -16,9 +16,12 @@ export const QUERY_ALL_USERS = `
 
 export const QUERY_USER_BY_ID_TYPE_WITH_POSTS = `
   ${USER_FIELDS}
-  query UserByIdTypeWithPosts($id: ID!) {
-    user(id: $id) {
-      ...UserFields
+  query UserByIdTypeWithPosts {
+    user(id: "dXNlcjox") {
+      id
+      name  
+      slug
+      databaseId
     }
   }
 `;
