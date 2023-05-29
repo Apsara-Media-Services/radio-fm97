8cb836c18cd77363d8865ab1117630d5b43e5a62
NySominea <sominea.ny77@gmail.com>
2023-05-29 19:09:54 +0700

    test: build

--- src/app/author/[slug]/page.tsx
+++ src/app/author/[slug]/page.tsx
@@ -1,29 +1,12 @@
-import { Container } from '@/components/common';
-import MainLayout from '@/components/layout/MainLayout';
-import InfiniteScrollPosts from '@/components/page/author/InfiniteScrollPosts';
 import { SkeletonPostItem } from '@/components/skeleton';
-import { UserService } from '@/services';
 import { IDynamicPage } from '@/types/page';
-import { isNil } from 'lodash';
-
-const userService = new UserService();
 
 const Author = async ({ params: { slug } }: IDynamicPage) => {
-  const user = await userService.findBySlugWithPosts(slug as string, {
-    variables: { first: 11 },
-  });
-
-  if (isNil(slug) || isNil(user)) return <></>;
-
   return (
-    <div>
-      <MainLayout>
-        <Container className="py-3 sm:py-5">
-          <SkeletonPostItem />
-          {/* <InfiniteScrollPosts user={user} slug={slug} /> */}
-        </Container>
-      </MainLayout>
-    </div>
+    <>
+      {slug}
+      <SkeletonPostItem />
+    </>
   );
 };
 
