9bb3a2136f08f742b108e4f80aafba99cfc46b40
sokmean-mptc <ngonsokmean@mptc.gov.kh>
2023-07-11 17:02:39 +0700

    #

--- src/components/layout/Player.tsx
+++ src/components/layout/Player.tsx
@@ -5,7 +5,7 @@ import WaveSurferPlayer from '@components/wavesurfer/WaveSurferPlayer';
 
 const Player = () => {
   const { control } = useAppContext();
-  if (!control.open || !control.lists.length) return;
+  if (!control.open || !control.lists.length) return <></>;
   return <WaveSurferPlayer control={control} />;
 };
 
