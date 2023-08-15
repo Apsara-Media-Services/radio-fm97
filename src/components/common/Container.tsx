babf401d1c2ab12fba7fce737ef75a1982d9a3dc
thailychan <129048311+thailychan@users.noreply.github.com>
2023-08-15 16:45:53 +0700

    added LiveV2

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -5,7 +5,7 @@ const Container = ({ children, className }: IComponentProps) => {
   return (
     <div
       className={classNames(
-        'max-w-xl md:max-w-2xl lg:max-w-5xl xl:max-w-7xl container mx-auto px-3 sm:px-5',
+        'max-w-xl md:max-w-2xl lg:max-w-5xl xl:max-w-7xl container mx-auto px-3 sm:px-5 z-10',
         className
       )}
     >
