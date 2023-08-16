75cb7a7785655391593d9f67ff0a41d7f860bda1
thailychan <129048311+thailychan@users.noreply.github.com>
2023-08-16 14:18:59 +0700

    fixed playing block and responsive

--- src/components/page/radio/RadioLiveV2.tsx
+++ src/components/page/radio/RadioLiveV2.tsx
@@ -63,12 +63,12 @@ const RadioLiveV2 = (props: any) => {
 
   return (
     <div className={className}>
-      <div className="min-h-[447px] flex items-center relative">
+      <div className="aspect-video lg:aspect-[16/4] flex items-center relative py-8">
         <div className="bg-img bg-black/80 absolute inset-0 z-10" />
         <div className={`bg-[url('http://localhost:3000/_next/image?url=https%3A%2F%2Fasset.ams.com.kh%2Fradiomedia%2Fwp-content%2Fuploads%2F2023%2F06%2F08101212%2Ffashion_time_1_cf6287fb89.webp&w=1920&q=75')] w-full h-full bg-center bg-cover bg-no-repeat absolute inset-0`} />
         <Container>
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 md:gap-20 z-20">
-            <div className="relative rounded-full aspect-square shadow-lg h-60 ml-auto">
+            <div className="relative rounded-full aspect-square shadow-lg h-60 mr-auto lg:mr-0 ml-auto">
               <FallbackImage
                 fill
                 src={program?.cover[0].url}
@@ -79,40 +79,23 @@ const RadioLiveV2 = (props: any) => {
             <div className='text-white'>
               {!isEmpty(program) && (
                 <>
-                  <div className="air-now">
-                    <div className={'text-white text-sm'}>
-                      <span className={'px-1 bg-ams-red'}>{'កំពុងផ្សាយ'}</span>
-                    </div>
-
-                    <div className="text-xl md:text-2xl my-1 font-semibold">
+                  <div className="air-now space-y-4 text-xl md:text-2xl font-semibold">
+                    <span className={'before:absolute before:-bottom-3 before:h-1 before:w-9 before:bg-ams-red relative'}>{'FM97'}</span>
+                    <h5>
                       {program?.title}
-                    </div>
-                    <div className="flex gap-1 items-center my-1 text-sm md:text-base">
-                      <ClockIcon className="h-4 w-4 md:h-5 md:w-5" />
+                    </h5>
+                    <div className="flex gap-1 items-center my-1">
+                      {/* <ClockIcon className="h-4 w-4 md:h-5 md:w-5" /> */}
                       <time>
                         {timestampTo12Hour(program?.startTimestamp)} ~{' '}
                         {timestampTo12Hour(program?.endTimestamp)}
                       </time>
                     </div>
-                    <div className="md:text-lg">{program?.description}</div>
-                    {_ReactPlayer.canPlay(radioLiveUrl) && (
-                      <ReactPlayer
-                        className="my-4"
-                        url={radioLiveUrl}
-                        playing={isPlaying}
-                        controls={true}
-                        height={50}
-                        width={'100%'}
-                        config={{ file: { forceAudio: true } }}
-                        onReady={handleEvent}
-                        onPlay={() => setIsPlaying(true)}
-                        onPause={() => setIsPlaying(false)}
-                      />
-                    )}
+                    {/* <div className="md:text-lg">{program?.description}</div> */}
                   </div>
                 </>
               )}
-              {!isEmpty(nextProgram) && (
+              {/* {!isEmpty(nextProgram) && (
                 <div className="up-next">
                   <div className="flex gap-1 items-center my-1 text-sm md:text-base">
                     <span>{' កម្មវិធីបន្ទាប់'}</span>
@@ -129,9 +112,30 @@ const RadioLiveV2 = (props: any) => {
                     </time>
                   </div>
                 </div>
-              )}
+              )} */}
             </div>
           </div>
+          <div className="mt-8">
+            {_ReactPlayer.canPlay(radioLiveUrl) && (
+              <ReactPlayer
+                className="my-4"
+                url={radioLiveUrl}
+                playing={isPlaying}
+                controls={true}
+                height={50}
+                width={'100%'}
+                config={{ file: { forceAudio: true } }}
+                onReady={handleEvent}
+                onPlay={() => setIsPlaying(true)}
+                onPause={() => setIsPlaying(false)}
+              />
+            )}
+          </div>
+        </Container>
+      </div>
+      <div className="program-options">
+        <Container>
+          
         </Container>
       </div>
     </div>
