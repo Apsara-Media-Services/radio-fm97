1aebf2f02ac908c298f454a22ca6dbf6d2bb0657
Chetra Chann <channchetra@gmail.com>
2024-07-05 19:50:11 +0700

    Revert NextConfig 🐛🦟🪰🐜🪲🐞

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
+if (!process.env.NEXT_PUBLIC_WORDPRESS_API_URL) {
+  throw new Error(`
+    Please provide a valid WordPress instance URL.
+    Add to your environment variables WORDPRESS_API_URL.
+  `);
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
   images: {
     domains: [
+      process.env.NEXT_PUBLIC_WORDPRESS_API_URL.match(/(http(?:s)?:\/\/)(.*)/)[2], // Valid WP Image domain.
       'api.ams.com.kh',
       'radio.amskh.co',
       'asset.ams.com.kh',
