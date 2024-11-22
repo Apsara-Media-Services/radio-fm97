e48e6de3b8b8a2cad885de64164d750e2f8f5f72
NySominea <sominea.ny77@gmail.com>
2024-11-22 10:36:32 +0700

    feat: add analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { ToastContainer } from '@/components/libs/ReactToastify';
 import { METADATA } from '@/constants/app';
 import { Metadata } from 'next';
 import { Kantumruy_Pro } from 'next/font/google';
+import Script from 'next/script';
 import 'react-toastify/dist/ReactToastify.css';
 
 const kantumruy_pro = Kantumruy_Pro({
@@ -31,6 +32,11 @@ export default function RootLayout({
           <ToastContainer />
           <NextNProgress color="#cf0a10" options={{ showSpinner: false }} />
         </AppProvider>
+        <Script
+          strategy="afterInteractive"
+          src="https://analytic.cloud.sovichetra.com/script.js"
+          data-website-id="7135a9cf-d04f-4ccf-979a-4ca2205650a8"
+        />
       </body>
     </html>
   );
